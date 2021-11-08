3655283da55fae417aa8ceca042984fecf12877c
darklord335 <37541842+darklord335@users.noreply.github.com>
2021-11-08 07:16:33 +0700

    Cập nhật các hàm xử lí trong Console
    Update Console function

---
+++ CrossingRoad/CrossingRoad/CConsole.cpp
@@ -0,0 +1,90 @@
+#include "CConsole.h"
+
+
+void FixConsoleWindow() {
+	HWND consoleWindow = GetConsoleWindow();
+	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+	SetWindowLong(consoleWindow, GWL_STYLE, style);
+}
+
+
+void GotoXY(int x, int y) {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+int inputKey() {
+	if (_kbhit()) {
+		int key = _getch();
+
+		if (key == 224) {
+			key = _getch();
+			return key + 1000;
+		}
+
+		return key;
+	}
+	else {
+		return key_none;
+	}
+
+	return key_none;
+}
+
+
+void clrscr() {
+	CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+	HANDLE hConsoleOut;
+	COORD Home = { 0, 0 };
+	DWORD dummy;
+
+	hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	GetConsoleScreenBufferInfo(hConsoleOut, &csbiInfo);
+
+	FillConsoleOutputCharacter(hConsoleOut, ' ', csbiInfo.dwSize.X * csbiInfo.dwSize.Y, Home, &dummy);
+	csbiInfo.dwCursorPosition.X = 0;
+	csbiInfo.dwCursorPosition.Y = 0;
+	SetConsoleCursorPosition(hConsoleOut, csbiInfo.dwCursorPosition);
+}
+
+
+void ShowConsoleCursor(bool showFlag) {
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_CURSOR_INFO cursorInfo;
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.dwSize = 1;
+	cursorInfo.dwSize = showFlag; // set the cursor visibility
+	SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+
+int whereX() {
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+		return csbi.dwCursorPosition.X;
+	return -1;
+}
+
+int whereY() {
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+		return csbi.dwCursorPosition.Y;
+	return -1;
+}
+
+void TextColor(int color) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void DisableMouse() {
+	DWORD prev_mode;
+	_In_ HANDLE hConsoleHandle;
+	_Out_ LPDWORD lpMode;
+	GetConsoleMode(hConsoleHandle, &prev_mode);
+	SetConsoleMode(hConsoleHandle, prev_mode & ~ENABLE_QUICK_EDIT_MODE);
+}
+
--- CrossingRoad/CrossingRoad/CConsole.h
+++ CrossingRoad/CrossingRoad/CConsole.h
@@ -1,7 +1,65 @@
+﻿#pragma once
 #include <iostream>
 #include <Windows.h>
 #include <fstream>
 #include <time.h>
-#pragma once
+#include <conio.h>
 
+using namespace std;
+
+
+#define ColorCode_Back			0
+#define ColorCode_DarkBlue		1
+#define ColorCode_DarkGreen		2
+#define ColorCode_DarkCyan		3
+#define ColorCode_DarkRed		4
+#define ColorCode_DarkPink		5
+#define ColorCode_DarkYellow	6
+#define ColorCode_DarkWhite		7
+#define ColorCode_Grey			8
+#define ColorCode_Blue			9
+#define ColorCode_Green			10
+#define ColorCode_Cyan			11
+#define ColorCode_Red			12
+#define ColorCode_Pink			13
+#define ColorCode_Yellow		14
+#define ColorCode_White			15
+
+#define default_ColorCode		7
+
+
+#define key_Up		1072
+#define key_Down	1080
+#define key_Left	1075
+#define key_Right	1077
+#define key_none	-1
+
+
+// Giá trị mặc định cho Game
+struct constVar {
+	static bool isMute;
+	static bool isEasy;
+};
+
+// Cố định màn hình chính
+void FixConsoleWindow();
+
+// Di chuyển tới ví trí con trỏ trong màn hình
+void GotoXY(int x, int y);
+
+// Nhận lệnh từ bàn phím người dùng
+int inputKey();
+
+// Lấy giá trị toạ độ 
+int whereX();
+int whereY();
+
+// Bôi màu cho các std::cout sau nó
+void TextColor(int color);
+
+// Hiện con trỏ chuột
+void ShowConsoleCursor(bool showFlag);
+
+// Ẩn con trỏ chuột
+void DisableMouse();
 
