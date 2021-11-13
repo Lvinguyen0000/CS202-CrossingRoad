daa578f508f53307f1bbdeb2edf0417d9db0f5d3
nlduy2402 <93860719+nlduy2402@users.noreply.github.com>
2021-11-13 13:52:40 +0700

    Update CCar.cpp

--- CrossingRoad/CrossingRoad/CCar.cpp
+++ CrossingRoad/CrossingRoad/CCar.cpp
@@ -1,1 +1,44 @@
 #include "CCar.h"
+CCar::CCar {
+  	a = new char* [3];
+	for (int i = 0; i < 3; i++) {
+		a[i] = new char[8];
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 8; j++) {
+			a[i][j] = ' ';
+		}
+	}
+	a[0][0] = '_';
+	a[0][1] = '/';
+	a[0][2] = '`';
+	a[0][3] = '`';
+	a[0][4] = '`';
+	a[0][5] = '\\';
+	a[0][6] = '_';
+	a[1][0] = '/';
+	a[1][7] = '\\';
+	a[1][0] = '_';
+	a[2][1] = '*';
+	a[1][2] = '_';
+	a[1][3] = '_';
+	a[1][4] = '_';
+	a[2][5] = '*';
+	a[1][6] = '_';
+}
+CCar::~CCar() {
+	for(int i=0; i< 3; i++) {
+		delete[] a[i];
+	}
+	delete a;
+}
+CCar::Move(int x, int y) {
+	if (x > 83 || y > 21) return;
+	for (int i = 0; i < 3; i++) {
+		gotoXY(x + i, y);
+		for (int j = 0; j < 8; j++) {
+			gotoXY(x + j, y + i);
+			cout << a[i][j];
+		}
+	}
+}
