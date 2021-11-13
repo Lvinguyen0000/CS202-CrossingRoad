7987e9fadcdb1aea759353e51627c9039e2140a9
tqthinh16 <thaiduong161@gmail.com>
2021-11-13 08:54:02 +0700

    Update CBird.h

--- CrossingRoad/CrossingRoad/CBird.h
+++ CrossingRoad/CrossingRoad/CBird.h
@@ -2,6 +2,14 @@
 #pragma once
 
 class CBird : public CAnimal {
-	public:
+public:
+	CBird();
+	~CBird();
+	void Move(int x, int y);
+	void Tell();
+	bool isOut(int x);
+	void Draw();
+private:
+	char** b;
 };
 
