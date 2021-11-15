68fabd22a8e2ca1c0bad932ee495635d612a4e69
darklord335 <37541842+darklord335@users.noreply.github.com>
2021-11-15 22:48:57 +0700

    Stuff

--- CrossingRoad/CrossingRoad/CObstacle.cpp
+++ CrossingRoad/CrossingRoad/CObstacle.cpp
@@ -7,17 +7,3 @@ CObstacle::CObstacle(int x = 0, int y = 0, bool direction = 0, bool light = 0, i
 	this->light = light;
 	this->speed = speed;
 }
-
-void CObstacle::setX(int x) {
-	mX = x;
-}
-void CObstacle::setY(int y) {
-	mY = y;
-}
-
-int CObstacle::getX() {
-	return mX;
-}
-int CObstacle::getY() {
-	return mY;
-}
--- CrossingRoad/CrossingRoad/CObstacle.h
+++ CrossingRoad/CrossingRoad/CObstacle.h
@@ -11,18 +11,7 @@ protected:
 public:
 	CObstacle(int x = 0, int y = 0, bool direction = 0, bool light = 0, int speed = 0);
 
-	void setX(int x) {
-		mX = x;
-	}
-	void setY(int y) {
-		mY = y;
-	}
-
-	int getX() {
-		return mX;
-	}
-	int getY() {
-		return mY;
-	}
+	virtual void Move() = 0;
+	virtual void Draw() = 0;
 };
 
