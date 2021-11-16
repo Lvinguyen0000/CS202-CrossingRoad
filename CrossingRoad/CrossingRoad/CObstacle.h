724ccfb20eba59253944c7e54b0a520b4ddefd16
darklord335 <37541842+darklord335@users.noreply.github.com>
2021-11-16 08:01:35 +0700

    Stuff

--- CrossingRoad/CrossingRoad/CObstacle.h
+++ CrossingRoad/CrossingRoad/CObstacle.h
@@ -5,8 +5,8 @@ using namespace std;
 class CObstacle {
 protected:
 	int mX, mY;
-	bool direction; //1:left 0 : right
-	bool light; //1:red 0 : green
+	bool direction; //1:left 0:right
+	bool light; //1:red 0:green
 	int speed; // truyền vào sleep
 public:
 	CObstacle(int x = 0, int y = 0, bool direction = 0, bool light = 0, int speed = 0);
