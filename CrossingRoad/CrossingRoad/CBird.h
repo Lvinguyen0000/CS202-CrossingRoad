3fd5c0b61df2ae0a15fd1433fbb7508ebcac6ac9
tqthinh16 <thaiduong161@gmail.com>
2021-11-16 23:18:55 +0700

    Update CBird.h

--- CrossingRoad/CrossingRoad/CBird.h
+++ CrossingRoad/CrossingRoad/CBird.h
@@ -9,10 +9,11 @@ class CBird : protected CObstacle {
 public:
 	CBird();
 	~CBird();
-	void Move(int x, int y);
+	void Move();
 	void Draw();
 	bool isOut(int x);
 private:
-	char** b;
+	//char** b;
+	string b[2];
 };
 
