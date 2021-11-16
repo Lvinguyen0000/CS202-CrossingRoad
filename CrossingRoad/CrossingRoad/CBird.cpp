b04ebcfffbbb2647123bac7b8135458b345d59af
tqthinh16 <thaiduong161@gmail.com>
2021-11-16 23:18:37 +0700

    Update CBird.cpp

--- CrossingRoad/CrossingRoad/CBird.cpp
+++ CrossingRoad/CrossingRoad/CBird.cpp
@@ -1,57 +1,74 @@
 #include "CBird.h"
 
-CBird::CBird() {
-	b = new char* [2];
-	for (int i = 0; i < 2; i++)
-		b[i] = new char[3];
-
-	b[0][0] = (char)254;
-	b[0][1] = ' ';
-	b[0][2] = (char)254;
-	b[1][0] = ' ';
-	b[1][1] = (char)223;
-	b[1][2] = ' ';
-}
+CBird::CBird() : CObstacle() {}
 
 void CBird::Draw() {
+	b[2] = {
+		"* *",
+		" * ",
+	};
+
+	GotoXY(mX, mY);
 	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			cout << b[i][j];
-		}
-		cout << endl;
+		cout << b[i];
 	}
 }
+
 CBird::~CBird() {
-	for (int i = 0; i < 3; i++)
-		delete[] b[i];
-	delete[] b;
+	delete b;
 }
 
-void CBird::Move(int x, int y) {
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			GotoXY(x + j, y + i + 1);
-			if (isOut(x + i + 3)) {
-				cout << ' ';
-				for (int k = 6; k < 9; k++) {
-					GotoXY(91, k);
-					cout << '|';
-				}
-			}
-			else {
-				cout << b[i][j];
+void CBird::Move() {
+	string del[3] = { " "," "," " };
+	string end[3] = { "    ","     ","     " };
+	if (direction) {
+		if (mX < 84) {
+			int oldX = mX, preX = mX + 1;
+			GotoXY(preX, mY);
+			Draw();
+			GotoXY(oldX, mY);
+			for (int i = 0; i < 2; i++) {
+				GotoXY(oldX, mY + i);
+				cout << del[i];
 			}
 		}
-		cout << endl;
+		else if (mX == 84) {
+			for (int i = 0; i < 2; i++) {
+				GotoXY(mX, mY + i);
+				cout << end[i];
+			}
+		}
+		else
+			return;
 	}
-	if (x != 1) {
-		for (int i = 1; i < 4; i++) {
-			GotoXY(x - 1, y + i);
-			cout << ' ';
+	else {
+		if (mX > 84) {
+			for (int i = 0; i < 2; i++) {
+				GotoXY(91, mY);
+				cout << "|";
+			}
+		}
+
+		else if (mX == 1) {
+			for (int i = 0; i < 2; i++) {
+				GotoXY(mX, mY + i);
+				cout << end[i];
+			}
+		}
+		else if (mX < 1) return;
+		
+		else {
+			int oldX = mX + 4, preX = mX - 1;
+			GotoXY(preX, mY);
+			Draw();
+			GotoXY(oldX, mY);
+			for (int i = 0; i < 2; i++) {
+				GotoXY(oldX, mY + i);
+				cout << del[i];
+			}
 		}
 	}
 }
-
 
 
 bool CBird::isOut(int x) {
