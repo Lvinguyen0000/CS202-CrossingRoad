6a687ef4dc92c7ac59445eaf33340b7cd37e9e6e
lvlnguyen00 <80013922+Lvinguyen0000@users.noreply.github.com>
2021-11-17 22:04:55 +0700

    Update CDinausor.cpp

--- CrossingRoad/CrossingRoad/CDinausor.cpp
+++ CrossingRoad/CrossingRoad/CDinausor.cpp
@@ -29,50 +29,61 @@ void CDinausor::Draw() {
 
 
 void CDinausor::Move() {
-	string del[3] = { " "," "," " };
+	if (direction) Left();
+	else Right();
+}
+
+void CDinausor::Left() {
 	string end[3] = { "    ","     ","     " };
+	string del = { ' ',' ',' ' };
+	int oldX = mX, oldY = this->mY;
+
+	mX--;
+
+	if (this->isOut()) {
+		for (int i = 0; i < 3; i++) {
+			GotoXY(oldX, oldY + i);
+			cout << end[i];
+		}
+		return;
+	}
+
+	this->Draw();
+	for (int i = 0; i < 3; i++) {
+		GotoXY(oldX + 6, oldY + i);
+		cout << del;
+	}
+	Sleep(this->speed);
+}
+
+void CDinausor::Right() {
+	string end[3] = { "    ","     ","     " };
+	string del = { ' ',' ',' ' };
+	int oldX = mX, oldY = mY;
+
+	mX++;
+
+	if (this->isOut()) {
+		for (int i = 0; i < 3; i++) {
+			GotoXY(oldX - 6, oldY + i);
+			cout << end[i];
+		}
+		return;
+	}
+
+	this->Draw();
+	for (int i = 0; i < 3; i++) {
+		GotoXY(oldX, oldY + i);
+		cout << del;
+	}
+	Sleep(this->speed);
+}
+
+bool CDinausor::isOut() {
 	if (direction) {
-		if (mX == 84) {
-			for (int i = 0; i < 3; i++) {
-				GotoXY(mX, mY + i);
-				cout << end[i];
-			}
-		}
-		else if (mX > 84) return;
-		else {
-			int oldX = mX, preX = mX + 1;
-			GotoXY(preX, mY);
-			Draw();
-			GotoXY(oldX, mY);
-			for (int i = 0; i < 3; i++) {
-				GotoXY(oldX, mY + i);
-				cout << del[i];
-			}
-		}
+		return(mX > 84 || mX < 2);
 	}
 	else {
-		if (mX == 1) {
-			for (int i = 0; i < 3; i++) {
-				GotoXY(mX, mY + i);
-				cout << end[i];
-			}
-		}
-		else if (mX < 1) return;
-		else if (mX > 84) {
-			for (int i = 0; i < 3; i++) {
-				GotoXY(91, mY);
-				cout << "|";
-			}
-		}
-		else {
-			int oldX = mX + 6, preX = mX - 1;
-			GotoXY(preX, mY);
-			Draw();
-			GotoXY(oldX, mY);
-			for (int i = 0; i < 3; i++) {
-				GotoXY(oldX, mY + i);
-				cout << del[i];
-			}
-		}
+		return (mX < 0 || mX > 84);
 	}
 }
