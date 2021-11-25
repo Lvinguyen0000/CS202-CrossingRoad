5fee9ceba8156cd9ef1192651605382ec3f418d9
nlduy2402 <93860719+nlduy2402@users.noreply.github.com>
2021-11-25 14:03:02 +0700

    Update

--- CrossingRoad/CrossingRoad/CTruck.h
+++ CrossingRoad/CrossingRoad/CTruck.h
@@ -4,7 +4,10 @@
 #include"CConsole.h"
 
 using namespace std;
-
+#define RIGHT_IN 0
+#define RIGHT_OUT 83
+#define LEFT_IN 83
+#define LEFT_OUT 6
 class CTruck: public CObstacle {
 public:
 	void Draw();
