8a7158b9ecb2d6f3ff5a34b27fc5bd576ef27428
darklord335 <37541842+darklord335@users.noreply.github.com>
2021-11-25 13:58:26 +0700

    Stuff

--- CrossingRoad/CrossingRoad/CCar.h
+++ CrossingRoad/CrossingRoad/CCar.h
@@ -2,6 +2,11 @@
 #include "CObstacle.h"
 #include "CConsole.h"
 
+#define LEFT_IN 80
+#define LEFT_OUT 3
+#define RIGHT_IN 0
+#define RIGHT_OUT 80
+
 class CCar : public CObstacle {
 public:
 	CCar();
